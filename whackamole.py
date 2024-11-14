58f6f52cd8a4452f697093b529880285b065c52e
Alina Vasina <alina.vasina@Alinas-MacBook-Air.local>
2024-11-13 22:57:14 -0500

    Add whackamole.py in github

--- whackamole.py
+++ whackamole.py
@@ -1,20 +1,39 @@
 import pygame
+import random
 
 
 def main():
     try:
         pygame.init()
-        # You can draw the mole with this snippet:
-        # screen.blit(mole_image, mole_image.get_rect(topleft=(x,y)))
+            ### field creation ###
         mole_image = pygame.image.load("mole.png")
         screen = pygame.display.set_mode((640, 512))
         clock = pygame.time.Clock()
+
+            ### mole initial position ###
+        mole_x = 1
+        mole_y = 1
+        mole_rect = mole_image.get_rect(topleft=(mole_x, mole_y))
         running = True
         while running:
             for event in pygame.event.get():
+                    ### not running ###
                 if event.type == pygame.QUIT:
                     running = False
-            screen.fill("light green")
+                    ## click reaction ###
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    if mole_rect.collidepoint(event.pos):
+                        mole_x = random.randrange(0, 20) * 32  # 20 columns
+                        mole_y = random.randrange(0, 16) * 32  # 16 rows
+                        mole_rect.topleft = (mole_x, mole_y)
+
+                ### field grid ###
+            screen.fill("aquamarine")
+            for i in range(1, 16):
+                pygame.draw.line(screen, 'teal', (0, 32 * i), (640, 32 * i))
+            for i in range(1, 20):
+                pygame.draw.line(screen, 'teal', (32 * i, 0), (32 * i, 512))
+            screen.blit(mole_image, mole_rect)
             pygame.display.flip()
             clock.tick(60)
     finally:
